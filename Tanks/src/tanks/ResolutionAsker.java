4dc58744f8f0932e5a4e3b2b96c6f3d403f4f888
Baron Alloway <baron@alloway.net>
2014-09-23 15:02:16 -0400

    updated resolution asker

--- Tanks/src/tanks/ResolutionAsker.java
+++ Tanks/src/tanks/ResolutionAsker.java
@@ -11,15 +11,39 @@ package tanks;
  * @author Baron
  */
 public class ResolutionAsker {
-    
+    private int width;
+    private int height;
+    String path;
     ResolutionAsker()
     {
     //open panel that will ask for resolution
     //4 types and return appropriate dimensions to main program
-        //getResolution returns resolution
-        //getDirectory returns directory
-        //etc
+    //etc
     }
+    
+    public int getWidth()
+    {
+        return width;
+    }
+    public void setWidth(int newWidth)
+    {
+        width = newWidth;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public void setHeight(int newHeight)
+    {
+        height = newHeight;
+    }        
+    public String getPath()
+    {
+        return path;
+    }
+    
+    
+           
     
     
     
