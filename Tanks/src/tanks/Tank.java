79a47242e97718580c940fdc7ca16805070b0200
Ryan <ryan.tam.24@gmail.com>
2014-09-26 11:47:11 -0400

    my tank variables set

--- Tanks/src/tanks/Tank.java
+++ Tanks/src/tanks/Tank.java
@@ -11,7 +11,26 @@ package tanks;
  * @author Baron
  */
 public class Tank {
-    int x;
-    int y;
+    int lives = 3;
     
+    double tankX;
+    double tankY;
+    double speed=2;
+    
+    boolean moveLeft=false;
+    boolean moveRight=false;
+    boolean moveUp=false;
+    boolean moveDown=false;
+    
+    boolean hitLeft=false;
+    boolean hitRight=false;
+    boolean hitUp=false;
+    boolean hitDown=false;
+    
+    int bulletMag=6;
+    int amo=bulletMag;
+    long reloadTime =600000000;
+    long triggerSpeed=300000000;
+    boolean fire=false;
+    boolean fired=false;
 }
